872cff17bf68b7e48756c416c1db17e209dd76c8
devgogogogo <imleo322@gmail.com>
2025-03-05 17:27:58 +0900

    귀찮아

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("추가수정");
         System.out.println("한번더 추가");
+        System.out.println("추가3");
     }
 }
