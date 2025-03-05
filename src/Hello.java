7071f6fef4363ea89e48fbf4d1ed14403ec84d80
devgogogogo <imleo322@gmail.com>
2025-03-05 19:00:29 +0900

    클래스 삭제

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("추가수정");
-        System.out.println("한번더 추가");
-        System.out.println("추가3");
-        System.out.println("추가4");
-    }
-}
